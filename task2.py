0ee97f4037ba6401f8cce96fd98fd239cc7299d9
kamila-velazquez533 <kamila.velazquez533@students.cmcss.net>
2025-12-18 09:12:43 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,14 @@
+allowance += dishes
+allowance += lawn
+allowance -= laundry
 
+# Week 2: Bonus week and purchase
+allowance += 5
+allowance += snack
+allowance -= game
+
+ # Week 3: Savings
+allowance /= 2
+
+# Print final allowance
+print(f'Allowance: ${allowance}')
