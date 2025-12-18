df8dd1ea9b08a653c58188cf03eee1354a6559ae
kamila-velazquez533 <kamila.velazquez533@students.cmcss.net>
2025-12-18 09:01:46 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,21 @@
+total_slices = party_pizza_mini + large + medium + people
+print(f'Total number of slices: {total_slices}')
 
+people += 1
+share = total_slices // people
+leftover = total_slices % people
+print(f'Each person gets: {share}')
+print(f'Leftover slices: {leftover}')
+
+people += 2
+share = total_slices // people
+leftover = total_slices % people
+print(f'Each person gets: {share}')
+print(f'Leftover slices: {leftover}')
+
+slices += party_pizza_mini
+share = slices // people
+leftover = slices % people
+print(f'Each person gets: {share}')
+print(f'Leftover slices: {leftover}')
+print("...for Mr. Hollow Leg")
